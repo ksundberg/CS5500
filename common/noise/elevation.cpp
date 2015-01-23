096e4ff7634e8fce084d1a98ec34d06a886c9d6c
Eric R Meissner <meissner.eric.7@gmail.com>
2015-01-23 16:55:18 -0700

    Create elevation.cpp

---
+++ common/noise/elevation.cpp
@@ -0,0 +1,7 @@
+#include "elevation.h"
+#include "PerlinNoise.h"
+
+double elevation (double x, double y, double minElevation, double maxElevation, PerlinNoise noise, int noiseDepth){
+  double weight = noise.turbulence2D(x, y, noiseDepth);
+  return minElevation * (1-weight) + maxElevation * weight;
+}
