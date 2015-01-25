d018ffd9ce2edfa832f8088dd7dbd008994b8bc4
waredavid <davidaware@gmail.com>
2015-01-24 22:01:43 -0700

    Beginning of Voxel engine.  Cmake files are untested

---
+++ client/src/voxel_rendering/block.cpp
@@ -0,0 +1,20 @@
+#include "block.h"
+
+Block::Block()
+{
+
+}
+
+Block::~Block()
+{
+
+}
+bool Block::IsActive()
+{
+	return mActive;
+}
+
+void Block::SetActive(bool active)
+{
+	mActive = active;
+}
