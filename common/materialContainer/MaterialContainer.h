20d11202d32d31e562f52d65db9d797390d87314
Soren <soren.rasmussen@aggiemail.usu.edu>
2015-02-08 18:45:02 -0700

    Reverting back a commit because changes broke the program

--- common/materialContainer/MaterialContainer.h
+++ common/materialContainer/MaterialContainer.h
@@ -13,145 +13,150 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <type_traits>
-#include "PerlinNoise.h"
-
 
 class Material
 {
-private:
-	std::string name;
-	double freq;
-public:
-	Material(std::string name, double freq):name(name),freq(freq){}
-	double getFreq(){ return freq ;}
-	std::string getName(){ return name; }
-	static Material Coal();
-	static Material Iron();
-	static Material Soil();
-	static Material Gravel();
-	static Material Sand();
-	static Material Diamond();
-	static Material Stone();
-	static Material Gold();
-	Material getMaterial(int x,int y,int z);
+ private:
+  std::string name;
+  double freq;
+
+ public:
+ Material(std::string name, double freq) : name(name), freq(freq) {}
+  double getFreq() { return freq; }
+  std::string getName() { return name; }
+  static Material Coal();
+  static Material Iron();
+  static Material Soil();
+  static Material Gravel();
+  static Material Sand();
+  static Material Diamond();
+  static Material Stone();
+  static Material Gold();
+  Material getMaterial();
 };
 
-
-template <class T> 
-class MaterialContainer
+template <class T> class MaterialContainer
 {
-protected:
-	
-	double PerlinNoise(int x, int y, int z,double smoothness)
-	{
-		static const int DEPTH=5;  
-		noise.turbulence3D(translate(x),translate(y),translate(z),DEPTH);
-	}
+ protected:
+  // temp PerlinNoise until intergration with project PerlinNoise
+  double PerlinNoise()
+  {
+    return (static_cast<double>(rand()) / static_cast<double>(RAND_MAX));
+  }
 
-	std::string name;
-	std::vector<T> contents;
-	double freq; // likelihood of a material to show up on a scale of (0,1).
-	double smoothness;
-	double offset;
-	double contentFreq; 
+  std::string name;
+  std::vector<T> contents;
+  double freq; // likelihood of a material to show up on a scale of (0,1).
+  double smoothness;
+  double contentFreq;
 
-	// set the probability of a material to get created
-	void setContentFreq()
-	{
-		contentFreq=0;
-		for(T c:contents) contentFreq+=c.getFreq();
-	}
+  // set the probability of a material to get created
+  void setContentFreq()
+  {
+    contentFreq = 0;
+    for (T c : contents)
+      contentFreq += c.getFreq();
+  }
 
-public:
-	// MaterialContainer constructors and operator
-	MaterialContainer(std::string name, std::vector<T> contents,double freq,double smoothness,double offset)
-		:name(name),contents(contents),freq(freq),smoothness(smoothness),offset(offset)
-	{
-		setContentFreq();
-	}
-	MaterialContainer(const MaterialContainer & MC)
-		:name(MC.name),contents(MC.contents),freq(MC.freq),smoothness(MC.smoothness),contentFreq(MC.contentFreq),offset(MC.offset){}
-	MaterialContainer& operator=(const MaterialContainer& mc){return *this;}
+ public:
+  // MaterialContainer constructors and operator
+  MaterialContainer(std::string name,
+                    std::vector<T> contents,
+                    double freq,
+                    double smoothness)
+    : name(name), contents(contents), freq(freq), smoothness(smoothness)
+  {
+    setContentFreq();
+  }
+  MaterialContainer(const MaterialContainer& MC)
+    : name(MC.name)
+    , contents(MC.contents)
+    , freq(MC.freq)
+    , smoothness(MC.smoothness)
+    , contentFreq(MC.contentFreq)
+    {
+    }
+  MaterialContainer& operator=(const MaterialContainer&) { return *this; }
 
-	// decides what element to return based on noise input
-	T getElement(double noise)
-	{
-		double tot = 0.0;
-		auto target = contentFreq*noise;
-		for (auto c : contents)
-		{
-			tot += c.getFreq();
-			if (tot >= target) return c;
-		}
-		return contents[0];
-	}
+  // decides what element to return based on noise input
+  T getElement(double noise)
+  {
+    double tot = 0.0;
+    auto target = contentFreq * noise;
+    for (auto c : contents)
+      {
+	tot += c.getFreq();
+	if (tot >= target) return c;
+      }
+    return contents[0];
+  }
 
-	// Returns frequency of an element
-	double getFreq(){ return freq ;}
+  // Returns frequency of an element
+  double getFreq() { return freq; }
 
-	// Returns rarity of an element
-	double getRarity() { return 1-freq; };
+  // Returns rarity of an element
+  double getRarity() { return 1 - freq; };
 
-	// Get the material type of a block at an (x,y,z) coordinate
-	virtual Material getMaterial(int x, int y, int z)
-	{
-		double noise = PerlinNoise(x, y, z,smoothness);
-		T content = getElement(noise);
-		return content.getMaterial(x, y, z);
-	}
-	double translate(int x){
-		return x*smoothness+offset;
-	}
+  // Get the material type of a block at an (x,y,z) coordinate
+  virtual Material getMaterial()
+  {
+    double noise = PerlinNoise();
+    T content = getElement(noise);
+    return content.getMaterial();
+  }
 };
 
-
-class Ground: public MaterialContainer<Material>{
-public:
-	Material getMaterial(int x, int y, int z);
-	static Ground sand();
-	static Ground oasis();
-
-	static Ground ironDeposits();
-	static Ground diamondDeposits();
-	static Ground goldDeposits();
-	static Ground noOre();
-
-	static Ground soil();
-	static Ground rockySoil();
-	static Ground sandySoil();
-
-	static const double GROUND_OFFSET;
-	static const double GROUND_SMOOTHNESS;
-
-	Ground(std::string name, std::vector<Material> contents,double freq)
-		:MaterialContainer(name,contents,freq,GROUND_SMOOTHNESS,GROUND_OFFSET){}
-};
-
-class Biome : public MaterialContainer < Ground >
+class Ground : public MaterialContainer<Material>
 {
-public:
-	static Biome sandyDesert();
-	static Biome mountain();
-	static Biome fertileFarmland(); 
+ public:
+  Material getMaterial();
+  static Ground sand();
+  static Ground oasis();
 
-	static const double BIOME_OFFSET;
-	static const double BIOME_SMOOTHNESS;
-	
-	Biome(std::string name, std::vector<Ground> contents,double freq)
-		:MaterialContainer(name,contents,freq,BIOME_SMOOTHNESS,BIOME_OFFSET){}
+  static Ground ironDeposits();
+  static Ground diamondDeposits();
+  static Ground goldDeposits();
+  static Ground noOre();
+
+  static Ground soil();
+  static Ground rockySoil();
+  static Ground sandySoil();
+  Ground(std::string name,
+         std::vector<Material> contents,
+         double freq,
+         double smoothness)
+    : MaterialContainer(name, contents, freq, smoothness)
+    {
+    }
 };
 
-class Biosphere : public MaterialContainer < Biome >
+class Biome : public MaterialContainer<Ground>
 {
-public:
-	static const double BIOSPHERE_OFFSET;
-	static const double BIOSPHERE_SMOOTHNESS;
-
-	Biosphere(std::string name, std::vector<Biome> contents,double freq)
-		:MaterialContainer(name,contents,freq,BIOSPHERE_SMOOTHNESS,BIOSPHERE_OFFSET){}
-
-	static Biosphere getBasicBiosphere();
+ public:
+  static Biome sandyDesert();
+  static Biome mountain();
+  static Biome fertileFarmland();
+  Biome(std::string name,
+        std::vector<Ground> contents,
+        double freq,
+        double smoothness)
+    : MaterialContainer(name, contents, freq, smoothness)
+    {
+    }
 };
 
+class Biosphere : public MaterialContainer<Biome>
+{
+ public:
+  Biosphere(std::string name,
+            std::vector<Biome> contents,
+            double freq,
+            double smoothness)
+    : MaterialContainer(name, contents, freq, smoothness)
+    {
+    }
+
+  static Biosphere getBasicBiosphere();
+};
 
 #endif
