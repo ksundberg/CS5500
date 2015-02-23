31a3428fd55e3097a66c8d92cf1fb9aabf0dfcb7
Drew Ingebretsen <drewying@gmail.com>
2015-02-22 19:25:05 -0700

    First run though of density map function

--- client/src/Grid.cpp
+++ client/src/Grid.cpp
@@ -1,11 +1,12 @@
 #include "Grid.hpp"
+#include <cfloat>
 
 const double COLOR_MAX = 255;
 
 void Grid::generateTerrain()
 {
   PerlinNoise perlin;
-  std::shared_ptr<matrix3d> noiseMap = perlin.createMatrix3D(width, height, depth, 5);
+  noiseMap = perlin.createMatrix3D(width, height, depth, 5);
 
   //perlin.smooth(noiseMap);
 
@@ -21,6 +22,75 @@ void Grid::generateTerrain()
       }
     }
   }
+}
+
+void Grid::drawDensityMap(wxDC& dc, int w, int h, double lowerBound, double upperBound){
+  auto densityMap = getDensityMap(lowerBound, upperBound);
+
+  dc.SetPen(wxPen(wxColor(0, 0, 0), 1));
+
+  int block_height = h / height;
+  int block_width = w / width;
+
+  for (int x = 0; x < width; x++)
+  {
+    for (int y = 0; y < height; y++)
+    {
+      double c = std::min(COLOR_MAX, (*densityMap)[x][y] * 255);
+      dc.SetBrush(wxColor(c, c, c));
+
+      dc.DrawRectangle(
+        x * block_width, y * block_height, block_width - 1, block_height - 1);
+    }
+  }
+
+}
+
+//A material is considered any voxel between two values in the Perlin Noise map.
+std::shared_ptr<matrix2d> Grid::getDensityMap(double lowerBound, double upperBound){
+  std::shared_ptr<matrix2d> returnMatrix = std::make_shared<matrix2d>();
+  //Get the count for each.
+  for (int x = 0; x < width; x++)
+  {
+    for (int y = 0; y < height; y++)
+    {
+      //Count the number of materials in the range.
+      double count = 0;
+      for (int z = 0; z < depth; z++)
+      {
+        double p = (*noiseMap)[x][y][z];
+        if (p > lowerBound && p < upperBound)
+        {
+          count++;
+        }
+      }
+      (*returnMatrix)[x][y] = count;
+    }
+  }
+
+  //Get the min and max 
+  double min = DBL_MAX;
+  double max = -DBL_MAX;
+
+  for (int x = 0; x < width; x++)
+  {
+    for (int y = 0; y < height; y++)
+    {
+      double p = (*returnMatrix)[x][y];
+      min = (p < min) ? p : min;
+      max = (p < max) ? p : max;
+    }
+  }
+
+  //Equalize the return matrix to values between 0-1 based on density
+  for (int x = 0; x < width; x++)
+  {
+    for (int y = 0; y < height; y++)
+    {
+      (*returnMatrix)[x][y] = ((*returnMatrix)[x][y] - min) / (max - min); 
+    }
+  }
+  return returnMatrix;
 }
 
 void Grid::draw(wxDC& dc, int w, int h, int z)
--- client/src/Grid.hpp
+++ client/src/Grid.hpp
@@ -14,10 +14,16 @@ public:
   void set(int x, int y, int z, const Point& v) { grid[idx(x, y, z)] = v; }
   int idx(int x, int y, int z) { return (x * width * height) + (y * height) + z; }
 
-  void generateTerrain();
   void draw(wxDC&, int w, int h, int z);
+  void drawDensityMap(wxDC& dc, int w, int h, double lowerBound, double upperBound);
+
+  void generateTerrain();
+  std::shared_ptr<matrix2d> getDensityMap(double lowerBound, double upperBound);
+  
+
 
   std::vector<Point> grid;
+  std::shared_ptr<matrix3d> noiseMap;
   int width;
   int height;
   int depth;
