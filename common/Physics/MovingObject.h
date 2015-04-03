447d0b68054f40562ab28148ed76cec2eeb7156f
tre4got <trevor@gotberg.net>
2015-04-03 09:07:25 -0600

    Update MovingObject.h

--- common/Physics/MovingObject.h
+++ common/Physics/MovingObject.h
@@ -1,1 +1,19 @@
 
+#ifndef MOVINGOBJECT_H
+#define MOVINGOBJECT_H
+
+#include "../util/vector3.h"
+
+class MovingObject
+{
+public:
+	MovingObject(Vector3 iniPosition);
+	double mass;          // in kg
+	Vector3 acceleration; // in m/s^2
+	Vector3 velocity;     // in m/s
+	Vector3 changeInPosition;
+	bool isMoving;
+	Vector3 Id;
+};
+
+#endif
